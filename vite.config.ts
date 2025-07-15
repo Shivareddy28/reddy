e28f2d179001e31ee388d84ff4d42693159ae6f6
Shivareddy28 <shivareddy007275@gmail.com>
2025-07-15 10:01:16 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
-  publicDir: "./static",
-  base: "./",
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom'],
-  },
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
-  }
-}));
+export default defineConfig({
+  plugins: [react()],
+});
+
